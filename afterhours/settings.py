28b7bcb54daaa825ed0a7bde16ea571f44cf5093
Sammy D <sammydowds1993@gmail.com>
2019-10-10 19:43:56 -0500

    Building platform for projects

--- afterhours/settings.py
+++ afterhours/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inquiry',
     'projects',
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    description = models.CharField(max_length=30)
+    STATUS_CHOICES = [
+        ('LATE', 'Late'),
+        ('ON TRACK', 'On Track'),
+        ('NEAR COMPLETIONS', 'Near Completion'),
+    ]
+    status = models.ChoiceField(choices = STATUS_CHOICES, default='ON TRACK')
+
+class Milestone(models.Model):
+    start = models.DateField()
+    end = models.DateField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+class Action(models.Model):
+    description = models.CharField(max_length=300)
+    status = models.BooleanField(default=False)
