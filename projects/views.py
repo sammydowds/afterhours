76b39c3978f973a4acc2618d5dd413aecdf7ec65
Sammy D <sammydowds1993@gmail.com>
2019-10-12 10:42:27 -0500

    Created project card draft

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def projects(request):
-    return render(request,'projects/projects.html')
+    return render(request,'projects/yourprojects.html')
 
 def yourprojects(request):
     return render(request, 'projects/yourprojects.html')
